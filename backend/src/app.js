fb52a2d4b812168d760dbb5b10d3f1bd61e20992
Manuel-Cobas <cobasguerram@gmail.com>
2022-02-27 18:04:40 -0400

    agregando cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,38 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const dotenv = require("dotenv").config();
 
 // Import Routes
 const UserRoutes = require("./routes/UserRoutes");
 
 // Initializations
 const app = express();
+const store = MongoDBStore({
+  uri: process.env.DB_HOST,
+  collection: "sessions",
+});
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    cookie:{
+      maxAge: (1000 * 60 * 60 * 24 * 7)
+    },
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+  })
+);
+
+
 
 // Config
 app.set("port", process.env.PORT || 3001);
@@ -22,7 +42,7 @@ app.set("port", process.env.PORT || 3001);
 app.use("/user", UserRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World", req.session);
 });
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+console.log(process.env.DB_HOST)
 
-mongoose.connect("mongodb://localhost:27017/portal-experto-blog")
+mongoose.connect(process.env.DB_HOST)
   .then(() => {
     app.listen(app.get('port'), () => {
       console.log(">>> DB is Connected");
